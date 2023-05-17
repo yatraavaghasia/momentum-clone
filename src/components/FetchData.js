38326b513a99990d2da02362d874e899c44ee96b
yatraavaghasia <113079616+yatraavaghasia@users.noreply.github.com>
2023-05-17 09:45:32 +0530

    Add files via upload

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -7,25 +7,29 @@ const PostComponent = () => {
         const fetchPosts = async() => {
             try {
                 const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-                setPosts(response.data);
+                const randomIndex = Math.floor(Math.random()*response.data.length);
+                setPosts(response.data[randomIndex]);
             } catch (error) {
                 console.log(error);
             }
         };
         fetchPosts();
     },[]);
-
+    const handleRefresh = () => {
+    window.location.reload(); // Refresh the page to get a new post
+  };
     return (
         <div>
             <h2>Posts</h2>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.body}</p>
-                    </li>
-                ))}
-            </ul>
+            {posts ? (
+                <div>
+                <h3>{posts.title}</h3>
+                <p>{posts.body}</p>
+                </div>
+            ):(
+                <p>Loading...</p>
+            )}
+            <button onClick={handleRefresh}>Refresh</button>
         </div>
     );
 };
